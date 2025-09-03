cba7acb295dc2ad4887eaaf224764edf898ee518
mohammedavez125 <mohammedavez125@gmil.com>
2025-09-03 13:01:57 +0530

    all

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,31 @@
+return{
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "lua_ls", "rust_analyzer" },
+            }
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config =  function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n','K',vim.lsp.buf.hover,{})
+        end
+    }
+}
+
