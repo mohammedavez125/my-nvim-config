2562dc4a53174ee526f71fc7ea61c5ddfe548d16
mohammedavez125 <mohammedavez125@gmil.com>
2025-09-14 15:37:21 +0530

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,8 +5,25 @@ return{
         "MunifTanjim/nui.nvim",
         "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
+    opts = {
+        filesystem = {
+            filtered_items = {
+             visible = true,
+             show_hidden_count = true,
+             hide_dotfiles = false,
+             hide_gitignored = false,
+             hide_by_name = {
+               -- '.git',
+               -- '.DS_Store',
+               -- 'thumbs.db',
+             },
+             never_show = {},
+            },
+        }
+    },
     lazy = false, -- neo-tree will lazily load itself
-    config = function()
+    config = function(_, opts)
+        require('neo-tree').setup(opts)
         vim.keymap.set('n','<C-n>',':Neotree toggle left<CR>',{})
     end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
-            ensure_installed = { "lua", "javascript", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+          ensure_installed = { "lua", "javascript", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
