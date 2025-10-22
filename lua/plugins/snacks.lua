32a6590d50609fb354801826f83d3e5e7d789720
mohammedavez125 <mohammedavez125@gmail.com>
2025-10-22 15:27:24 +0530

    snacks fixed

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,7 +1,11 @@
 return{
   "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
   opts = {
     dashboard = {
+      enabled = true,
       preset = {
         pick = function(cmd, opts)
           return LazyVim.pick(cmd, opts)()
@@ -18,12 +22,12 @@ return{
         ---@type snacks.dashboard.Item[]
         keys = {
           { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "x", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = " ", key = "n", desc = "Neo tree", action = ":Neotree toggle left" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
